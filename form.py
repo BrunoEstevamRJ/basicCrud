3b2b96a99199ade50c537fc5d1ef10dfbfe53c3d
Bruno Estevam <67297226+BrunoEstevamRJ@users.noreply.github.com>
2020-06-30 22:51:37 -0300

    Add files via upload

---
+++ form.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Transacao
+
+
+class TransacaoForm(ModelForm):
+    class Meta:
+        model = Transacao
+        fields = ['data', 'descricao', 'valor', 'observacoes', 'categoria']
+        
---
+++ models.py
@@ -0,0 +1,27 @@
+﻿from django.db import models
+
+
+# Create your models here.
+class Categoria(models.Model):
+    dt_criacao = models.DateTimeField()
+    nome = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.nome
+    
+        
+class Transacao(models.Model):
+    data = models.DateTimeField()
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=7,decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True,blank=True)   
+    
+    def __str__(self):
+            return self.descricao        
+    
+    class Meta:
+        verbose_name_plural = 'Transações'
+        
+
